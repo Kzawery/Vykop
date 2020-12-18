7cc713d800a94eab5081cbfb395bde87efce5c2a
Kondrat <kondrattrunowicz@wp.pl>
2020-12-18 20:42:41 +0100

    inicjalizacja nowego frontendu, dodanie panelu logowania

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,36 @@
-
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 
-import {LoginComponent} from './components/login/login.component';
-import {RegisterComponent} from './components/register/register.component';
-import {HomeComponent} from './components/home/home.component';
-import {BrowserModule} from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './shared/material.module';
-import {FormsModule} from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import {MatCardModule} from '@angular/material/card';
 import {AppRoutingModule} from './app-routing.module';
-import {HomeModule} from './components/home/home.module';
-import { PostComponent } from './components/post/post.component';
-import {HttpClientModule} from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    PostComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    MatCardModule,
     AppRoutingModule,
-    FormsModule,
-    HomeModule,
-    HttpClientModule,
-    MatInputModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,66 +1,24 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {Router} from '@angular/router';
-import {Subscription} from 'rxjs';
-import {MatDialog} from '@angular/material/dialog';
-import {RegisterComponent} from '../register/register.component';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  subs: Subscription[] = [];
-  text1 = '';
-  constructor(private router: Router, private matDialog: MatDialog, private http: HttpClient) {
-  }
+export class LoginComponent implements OnInit {
 
+  isLoading: boolean = false;
+
+  loginForm = new FormGroup({
+    form_basic_username: new FormControl('', [Validators.required]),
+    form_basic_password: new FormControl('', [Validators.required])
+  });
+  constructor() { }
+  onSave(){
+    this.isLoading = true;
+  }
   ngOnInit(): void {
-
   }
 
-  ngOnDestroy(): void {
-    this.subs.map(s => s.unsubscribe());
-  }
-
-  login(form: NgForm): void {
-    const {email, password} = form.value;
-
-
-    // if (!form.valid) {
-    //   return;
-    // }
-    this.http.get<string>('http://localhost:8080/posts' ).subscribe(data => {
-      this.text1 = data;
-    });
-
-    console.log(this.text1);
-
-    form.resetForm();
-  }
-
-  openRegister(): void {
-    const dialogRef = this.matDialog.open(RegisterComponent, {
-      role: 'dialog',
-      height: '480px',
-      width: '480px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      const {fname, lname, email, password, avatar} = result;
-
-      if (result !== undefined) {
-        // this.authService.SignUp(email, password, fname, lname, avatar);
-      }
-
-      return;
-    });
-  }
-
-
-}
-interface Credentials {
-  email: string;
-  password: string;
 }
