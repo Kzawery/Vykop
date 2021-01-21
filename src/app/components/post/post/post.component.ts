54a4706e1ac89755c03d87b84b7afb2721a9d758
Kondrat <kondrattrunowicz@wp.pl>
2021-01-21 19:39:53 +0100

    naprawiono komenatrze w pustach, dodano opcje usuwania

--- src/app/components/post/post/post.component.ts
+++ src/app/components/post/post/post.component.ts
@@ -6,7 +6,9 @@ import {Post} from '../../../models/post';
 import {PostService} from '../../../services/post.service';
 import {AuthenticationService} from '../../../services/authentication.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
-
+import {Comment} from '../../../models/comment'
+import {DeleteDialogComponent} from '../../delete-dialog/delete-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -22,7 +24,7 @@ export class PostComponent implements OnInit {
   id_post: any;
   post: Post = new Post();
   comment: string;
-  constructor(private ngZone: NgZone, private route: ActivatedRoute, public postService: PostService, public auth: AuthenticationService, private _snackBar: MatSnackBar) { }
+  constructor(private ngZone: NgZone, private route: ActivatedRoute, public postService: PostService, public auth: AuthenticationService, private _snackBar: MatSnackBar, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( paramMap => {
@@ -40,16 +42,44 @@ export class PostComponent implements OnInit {
     this.fetchMore();
   }
 
-  likeBtnClick(event) {
-    console.log(event);
+  likeBtnClick(element: Comment) {
+    this.postService.upvoteComment(element.id).subscribe(resp => {
+      this.refresh();
+      this._snackBar.open('You like this comment', 'hide',  {
+        duration: 2000,
+      });
+    });
   }
 
   fetchMore(): void {
   }
 
+  openDialog(element: Comment): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '250px',
+      disableClose: true,
+      hasBackdrop: true,
+      data: {id: element.id, model: 'Comment'},
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.deleteComment(element);
+      }
+    });
+  }
+
+  deleteComment(element: Comment): void {
+    this.postService.deleteComment(element.id).subscribe(resp => {
+      this.refresh();
+      this._snackBar.open('You have deleted your comment', 'hide', { duration: 2000
+  });
+});
+}
   addComment(): void {
     this.postService.addComment(this.id_post, this.comment).subscribe( resp => {
       this.refresh();
+      this.addCommentField = false;
+      this.comment = '';
       this._snackBar.open('Comment have been added', 'hide',  {
         duration: 2000,
       });
