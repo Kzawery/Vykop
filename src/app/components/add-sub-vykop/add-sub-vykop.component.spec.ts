c05bd97186d1495efdc02265a5bc3e04e09ba75a
Kondrat Trunowicz <kondrattrunowicz@gmail.com>
2021-05-01 21:36:48 +0200

    Naprawiono podstawowe testy dla AddSubVykopComponent

--- src/app/components/add-sub-vykop/add-sub-vykop.component.spec.ts
+++ src/app/components/add-sub-vykop/add-sub-vykop.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddSubVykopComponent } from './add-sub-vykop.component';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('AddSubVykopComponent', () => {
   let component: AddSubVykopComponent;
@@ -8,7 +10,17 @@ describe('AddSubVykopComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddSubVykopComponent ]
+      declarations: [ AddSubVykopComponent ],
+      providers: [
+        {
+        provide: MatDialog,
+        useValue: {}
+      },
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        }],
+      imports: [HttpClientModule]
     })
     .compileComponents();
   });
