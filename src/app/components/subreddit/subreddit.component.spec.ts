441c035f62addf455464bf6e9d8219cb5a7fb346
Kondrat Trunowicz <kondrattrunowicz@gmail.com>
2021-05-02 13:11:41 +0200

    Naprawiono podstawowy test dla Subreddita i WebSocket

--- src/app/components/subreddit/subreddit.component.spec.ts
+++ src/app/components/subreddit/subreddit.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { convertToParamMap} from '@angular/router';
 import { SubredditComponent } from './subreddit.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {Observable, of} from 'rxjs';
 
 describe('SubredditComponent', () => {
   let component: SubredditComponent;
@@ -8,7 +13,30 @@ describe('SubredditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SubredditComponent ]
+      declarations: [ SubredditComponent ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { paramMap: Observable.create(convertToParamMap({id: 1})) }
+        },
+        {
+          provide: Router,
+          useValue: {}
+        },
+        {
+          provide: MatSnackBar,
+          useValue: {}
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+        {
+          provide: MatDialog,
+          useValue: {}
+        },
+      ],
+      imports: [ HttpClientModule ]
     })
     .compileComponents();
   });
--- src/app/services/websocket.service.spec.ts
+++ src/app/services/websocket.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WebsocketService } from './websocket.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('WebsocketService', () => {
   let service: WebsocketService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule ]
+    });
     service = TestBed.inject(WebsocketService);
   });
 
