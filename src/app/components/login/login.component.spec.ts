820f9841401ca119ce275541423b445801b8e035
Kondrat Trunowicz <kondrattrunowicz@gmail.com>
2021-05-04 11:06:25 +0200

    Dodano testy dla LoginComponent

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,13 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ActivatedRoute, Router} from '@angular/router';
+import {AbstractControl, FormBuilder, FormGroup} from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let routerSpy = {navigate: jasmine.createSpy('navigate')};
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
@@ -17,8 +18,9 @@ describe('LoginComponent', () => {
       },
         {
           provide: Router,
-          useValue: {}
-        }],
+          useValue: routerSpy
+        },
+        {provide: FormBuilder}],
       imports: [HttpClientModule]
     })
     .compileComponents();
@@ -33,4 +35,39 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('routerTest', () => {
+    it('should go to register', () => {
+      component.toRegister();
+      expect (routerSpy.navigate).toHaveBeenCalledWith(['/register']);
+    });
+  });
+  describe('createForm', () => {
+    describe('login control', () => {
+      it('username should fail required validator', () => {
+        const form: FormGroup = component.loginForm;
+        const control: AbstractControl = form.get('form_basic_username');
+        control.setValue(null);
+        expect(control.hasError('required')).toBeTruthy();
+      });
+      it('password should fail required validator', () => {
+        const form: FormGroup = component.loginForm;
+        const control: AbstractControl = form.get('form_basic_password');
+        control.setValue(null);
+        expect(control.hasError('required')).toBeTruthy();
+      });
+      it('username should pass required validator', () => {
+        const form: FormGroup = component.loginForm;
+        const control: AbstractControl = form.get('form_basic_username');
+        control.setValue('user');
+        expect(control).toBeTruthy();
+      });
+      it('password should pass required validator', () => {
+        const form: FormGroup = component.loginForm;
+        const control: AbstractControl = form.get('form_basic_password');
+        control.setValue('password');
+        expect(control).toBeTruthy();
+      });
+    });
+  });
+
 });
